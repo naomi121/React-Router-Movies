edc69cd1fdfea717de6d82b202e983ad4ecf8bb4
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-28 14:34:44 -0500

    added Link to movies in MovieList.js in line 28

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +25,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link to={`movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
@@ -40,7 +46,7 @@ function MovieDetails({ movie }) {
       </div>
       <h3>Actors</h3>
 
-      {stars.map(star => (
+      {stars && stars.map(star => (
         <div key={star} className="movie-star">
           {star}
         </div>
